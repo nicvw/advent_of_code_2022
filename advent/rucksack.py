a540f7e13afc48374f7c38d857cdce7588992b8e
Nicholas von Waltsleben <waltsleben@gmail.com>
2022-12-03 12:43:42 +0200

    add day 3 comments

--- advent/rucksack.py
+++ advent/rucksack.py
@@ -1,3 +1,7 @@
+#
+# twas the third day of xmas...
+#
+
 from functools import reduce
 import string
 from typing import List, Tuple
@@ -16,20 +20,24 @@ def split(backpack: str) -> Tuple[str, str]:
     return backpack[:split_at], backpack[split_at:]
 
 def find_overlap(first: str, second: str) -> str:
+    """Find the overlapping characters in a string"""
     s1 = set(list(first))
     s2 = set(list(second))
     return "".join(list(s1 & s2))
 
 def reorder_rucksacks():
+    """Path One"""
     overlap: List[str] = []
     for backpack in iterate_over_data('rucksack.txt'):
         overlap.append(find_overlap(*split(backpack)))
     return sum([priority(x) for x in overlap])
 
 def overlap_wrapper(*args: str) -> str:
+    """Wrapper for finding the overlap in more than two strings"""
     return reduce(find_overlap, args)
 
 def auth_stickers() -> int:
+    """Part Two"""
     rucksacks = iterate_over_data('rucksack.txt')
     overlap: List[str] = []
     for r1, r2, r3 in zip(rucksacks, rucksacks, rucksacks):
